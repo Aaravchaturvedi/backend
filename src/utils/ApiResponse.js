a6eec944becb0256fc01136e4c24193dc73a4f08
Aarav Chaturvedi <aaravchaturvedi66@gmail.com>
2025-09-06 18:53:53 +0530

    utils code discussion

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse{
+    constructor(statuCode,data,message = "Success"){
+
+        this.statuCode = statuCode
+        this.data =data
+        this.message=message
+        this.success = statuCode < 400
+    }
+}
